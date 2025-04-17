d9ffa3e5cbfdfe969a511b476ad9d48c079ebecf
Tang Bo Hao <tech@btang.cn>
2025-04-17 17:37:43 +0800

    feat: refactor flow utils

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -1,4 +1,4 @@
-import { type Network, networkName } from "./config";
+import { type Network, networkName, networks } from "./config";
 import { FlowConnector, FlowWallet } from "./flow";
 import type { FlowBlockchainContext } from "./types";
 
@@ -7,12 +7,15 @@ import flowJSON from "../../flow.json" assert { type: "json" };
 export async function buildBlockchainContext(
 	network: Network = networkName,
 ): Promise<FlowBlockchainContext> {
-	const connector = new FlowConnector(flowJSON, network);
-	let wallet: FlowWallet | undefined = undefined;
-	try {
-		wallet = new FlowWallet(connector);
-	} catch (_e) {
-		// No need to log error here, it's probably because the wallet is not set
-	}
-	return { connector, wallet };
+  if (!networks.includes(network)) {
+    throw new Error(`Unsupported network: ${network}`);
+  }
+  const connector = new FlowConnector(flowJSON, network);
+  let wallet: FlowWallet | undefined = undefined;
+  try {
+    wallet = new FlowWallet(connector);
+  } catch (_e) {
+    // No need to log error here, it's probably because the wallet is not set
+  }
+  return { connector, wallet };
 }
--- src/utils/flow/flow.connector.ts
+++ src/utils/flow/flow.connector.ts
@@ -4,156 +4,148 @@ import type { Account, TransactionStatus } from "@onflow/typedefs";
 import type { Network } from "../config";
 import type { Authz, IFlowScriptExecutor } from "../types";
 
-let isGloballyInited = false;
-let globallyPromise: Promise<void> | null = null;
-
 export class FlowConnector implements IFlowScriptExecutor {
-	/**
-	 * Initialize the Flow SDK
-	 */
-	constructor(
-		private readonly flowJSON: object,
-		public readonly network: Network,
-		private readonly defaultRpcEndpoint: string | undefined = undefined,
-	) {}
+  private isInited = false;
+  private locallyInitedPromise: Promise<void> | null = null;
 
-	/**
-	 * Get the RPC endpoint
-	 */
-	get rpcEndpoint() {
-		switch (this.network) {
-			case "mainnet":
-				return this.defaultRpcEndpoint ?? "https://mainnet.onflow.org";
-			case "testnet":
-				return this.defaultRpcEndpoint ?? "https://testnet.onflow.org";
-			default:
-				throw new Error(`Network type ${this.network} is not supported`);
-		}
-	}
+  /**
+   * Initialize the Flow SDK
+   */
+  constructor(
+    private readonly flowJSON: object,
+    public readonly network: Network,
+    private readonly defaultRpcEndpoint: string | undefined = undefined,
+  ) {}
 
-	/**
-	 * Initialize the Flow SDK
-	 */
-	async onModuleInit() {
-		if (isGloballyInited) return;
+  /**
+   * Get the RPC endpoint
+   */
+  get rpcEndpoint() {
+    switch (this.network) {
+      case "mainnet":
+        return this.defaultRpcEndpoint ?? "https://mainnet.onflow.org";
+      case "testnet":
+        return this.defaultRpcEndpoint ?? "https://testnet.onflow.org";
+      default:
+        throw new Error(`Network type ${this.network} is not supported`);
+    }
+  }
 
-		const cfg = fcl.config();
-		// Required
-		cfg.put("flow.network", this.network);
-		// Set soft finality
-		cfg.put("fcl.experimental.softFinality", true);
-		// Set the maximum of gas limit
-		cfg.put("fcl.limit", 9999);
-		// Set the RPC endpoint
-		cfg.put("accessNode.api", this.rpcEndpoint);
-		// Load Flow JSON
-		await cfg.load({ flowJSON: this.flowJSON as Record<string, unknown> });
+  get fcl() {
+    return fcl;
+  }
 
-		isGloballyInited = true;
-	}
+  /**
+   * Initialize the Flow SDK
+   */
+  async onModuleInit() {
+    if (this.isInited) return;
 
-	/**
-	 * Ensure the Flow SDK is initialized
-	 */
-	private async ensureInited() {
-		if (isGloballyInited) return;
-		if (!globallyPromise) {
-			globallyPromise = this.onModuleInit();
-		}
-		return await globallyPromise;
-	}
+    const cfg = fcl.config();
+    // Required
+    cfg.put("flow.network", this.network);
+    // Set soft finality
+    cfg.put("fcl.experimental.softFinality", true);
+    // Set the maximum of gas limit
+    cfg.put("fcl.limit", 9999);
+    // Set the RPC endpoint
+    cfg.put("accessNode.api", this.rpcEndpoint);
 
-	/**
-	 * Get account information
-	 */
-	async getAccount(addr: string): Promise<Account> {
-		await this.ensureInited();
-		return await fcl.send([fcl.getAccount(addr)]).then(fcl.decode);
-	}
+    await cfg.load({ flowJSON: this.flowJSON as Record<string, unknown> }, { ignoreConflicts: true });
 
-	/**
-	 * General method of sending transaction
-	 */
-	async sendTransaction(code: string, args: ArgsFn, mainAuthz: Authz) {
-		await this.ensureInited();
-		if (typeof mainAuthz !== "undefined") {
-			return await fcl.mutate({
-				cadence: code,
-				args: args,
-				proposer: mainAuthz,
-				payer: mainAuthz,
-				authorizations: [mainAuthz],
-			});
-		}
-		return await fcl.mutate({
-			cadence: code,
-			args: args,
-		});
-	}
+    this.isInited = true;
+  }
 
-	/**
-	 * Get chain id
-	 */
-	async getChainId() {
-		await this.ensureInited();
-		return await fcl.getChainId();
-	}
+  /**
+   * Ensure the Flow SDK is initialized
+   */
+  private async ensureInited() {
+    if (this.isInited) return;
+    if (!this.locallyInitedPromise) {
+      this.locallyInitedPromise = this.onModuleInit();
+    }
+    return await this.locallyInitedPromise;
+  }
 
-	/**
-	 * Get transaction status
-	 */
-	async getTransactionStatus(
-		transactionId: string,
-	): Promise<TransactionStatus> {
-		await this.ensureInited();
-		return await fcl.tx(transactionId).onceExecuted();
-	}
+  /**
+   * Get account information
+   */
+  async getAccount(addr: string): Promise<Account> {
+    await this.ensureInited();
+    return await fcl.send([fcl.getAccount(addr)]).then(fcl.decode);
+  }
 
-	async onceTransactionSealed(
-		transactionId: string,
-	): Promise<TransactionStatus> {
-		await this.ensureInited();
-		return fcl.tx(transactionId).onceSealed();
-	}
+  /**
+   * General method of sending transaction
+   */
+  async sendTransaction(code: string, args: ArgsFn, mainAuthz: Authz) {
+    await this.ensureInited();
+    if (typeof mainAuthz !== "undefined") {
+      return await fcl.mutate({
+        cadence: code,
+        args: args,
+        proposer: mainAuthz,
+        payer: mainAuthz,
+        authorizations: [mainAuthz],
+      });
+    }
+    return await fcl.mutate({
+      cadence: code,
+      args: args,
+    });
+  }
 
-	async onceTransactionExecuted(
-		transactionId: string,
-	): Promise<TransactionStatus> {
-		await this.ensureInited();
-		return fcl.tx(transactionId).onceExecuted();
-	}
+  /**
+   * Get chain id
+   */
+  async getChainId() {
+    await this.ensureInited();
+    return await fcl.getChainId();
+  }
 
-	/**
-	 * Get block object
-	 * @param blockId
-	 */
-	async getBlockHeaderObject(blockId: string) {
-		await this.ensureInited();
-		return await fcl
-			.send([fcl.getBlockHeader(), fcl.atBlockId(blockId)])
-			.then(fcl.decode);
-	}
+  /**
+   * Get transaction status
+   */
+  async getTransactionStatus(transactionId: string): Promise<TransactionStatus> {
+    await this.ensureInited();
+    return await fcl.tx(transactionId).onceExecuted();
+  }
 
-	/**
-	 * Send script
-	 */
-	async executeScript<T>(
-		code: string,
-		args: ArgsFn,
-		defaultValue: T,
-	): Promise<T> {
-		await this.ensureInited();
-		try {
-			const queryResult = await fcl.query({
-				cadence: code,
-				args,
-			});
-			return (queryResult as T) ?? defaultValue;
-		} catch (e) {
-			console.error(e);
-			return defaultValue;
-		}
-	}
+  async onceTransactionSealed(transactionId: string): Promise<TransactionStatus> {
+    await this.ensureInited();
+    return fcl.tx(transactionId).onceSealed();
+  }
+
+  async onceTransactionExecuted(transactionId: string): Promise<TransactionStatus> {
+    await this.ensureInited();
+    return fcl.tx(transactionId).onceExecuted();
+  }
+
+  /**
+   * Get block object
+   * @param blockId
+   */
+  async getBlockHeaderObject(blockId: string) {
+    await this.ensureInited();
+    return await fcl.send([fcl.getBlockHeader(), fcl.atBlockId(blockId)]).then(fcl.decode);
+  }
+
+  /**
+   * Send script
+   */
+  async executeScript<T>(code: string, args: ArgsFn, defaultValue: T): Promise<T> {
+    await this.ensureInited();
+    try {
+      const queryResult = await fcl.query({
+        cadence: code,
+        args,
+      });
+      return (queryResult as T) ?? defaultValue;
+    } catch (e) {
+      console.error(e);
+      return defaultValue;
+    }
+  }
 }
 
 export default FlowConnector;
