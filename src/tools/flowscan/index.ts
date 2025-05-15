73e910e267a32d1d2c22d4190af2a451450d497a
Caos <caosbad@gmail.com>
2025-05-15 17:19:15 +0800

    feat: add query transaction tool

---
+++ src/tools/flowscan/index.ts
@@ -0,0 +1,55 @@
+import type { ToolRegistration } from "@/types/tools.js";
+import { type GetTransactionSchema, getTransactionInfoSchema } from "./schema.js";
+import axios from 'axios'
+
+export const getTransaction = async (args: GetTransactionSchema): Promise<any> => {
+  const { txid } = args;
+  try {
+
+    let url = `https://evm.flowscan.io/api/v2/transactions/${txid}`
+
+    const res = await axios.get(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json;version=20230302',
+      },
+    })
+
+    return res.data
+
+  } catch (error) {
+    throw new Error(`Error fetching contract: ${error instanceof Error ? error.message : String(error)}`);
+  }
+};
+
+export const getTransactionTool: ToolRegistration<GetTransactionSchema> = {
+  name: "get_transaction_info",
+  description: "Get transaction info with trxid on flowscan.io",
+  inputSchema: getTransactionInfoSchema,
+  handler: async (args: GetTransactionSchema) => {
+    try {
+      const parsedArgs = getTransactionInfoSchema.parse(args);
+      const result = await getTransaction(parsedArgs);
+      return {
+        content: [
+          {
+            type: "text",
+            text: result,
+          },
+        ],
+      };
+    } catch (error) {
+      console.error("Error in getTransactionTool handler:", error);
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Error: ${(error as Error).message}`,
+          },
+        ],
+        isError: true,
+      };
+    }
+  },
+};
+
