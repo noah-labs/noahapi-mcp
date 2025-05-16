b33ee936bed22ddc97a9a23cf476018932963797
lmcmz <lmcmze@gmail.com>
2025-05-16 12:02:33 +1000

    add gloabal error handler

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,12 @@ const useSSE = args.includes("--sse");
 const server = createServer();
 startServer(server, useSSE);
 
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+console.error('Unhandled Rejection:', reason);
+});
+
 export default server;
